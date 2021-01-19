2be935ccbc2eb6c931ec931b1a91c214db67c4a6
Aishwarya0710 <aishwaryamagre@gmail.com>
2021-01-19 10:55:21 +0530

    chatot

--- main-app/src/App.js
+++ main-app/src/App.js
@@ -7,9 +7,16 @@ import Testing from "./Components/Testing";
 import NavBarComponent from "./Components/NavBarComponent";
 import Error from "./Components/Error"
 import DynamicQuotes from "./Components/DynamicQuotes";
+import SimpleForm from "./Components/SimpleForm"
+import bot from "../src/images/Riva_Icon.gif";
 
+import React, { useEffect, useState } from "react";
 function App() {
- 
+    let [showChat, setShowChat] = useState(false);
+
+    const startChat = () => { setShowChat(true); }
+    const hideChat = () => { setShowChat(false); }
+
   return (
     <div className="app">
       <NavBarComponent/>
@@ -38,8 +45,21 @@ function App() {
             component={Testing}
           ></Route>
           <Route component={Error}/>
+                 
         </Switch>
-      </Router>
+          </Router>
+          <div className="bot">
+              <div style={{ display: showChat ? "" : "none" }}>
+                  <SimpleForm></SimpleForm>
+              </div>
+              {/* <div> {showChat ? <SimpleForm></SimpleForm> : null} </div> */}
+              <div>
+                  {!showChat
+                      ? 
+                      <button className="btn" onClick={() => startChat()}>Click to chat... </button>
+                      : <button className="btn" onClick={() => hideChat()}>Click to hide... </button>}
+              </div>
+          </div>
     </div>
   );
 }
