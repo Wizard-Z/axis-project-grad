5a5aeadb4a8b411cd63b449d53d9ac904b235270
Sourabh <sourabhchoubey007@gmail.com>
2021-01-19 11:32:57 +0530

    (update)config-via-csv

--- admin-panel/src/components/testing.js
+++ admin-panel/src/components/testing.js
@@ -1,8 +1,70 @@
+// const fs = require('fs')
+// const readline = require('readline-sync')
 
-let csvToJson = require('convert-csv-to-json');
 
-let json = csvToJson.getJsonFromCsv("myInputFile.csv");
-console.log(JSON.stringify(json))
-// for(let i=0; i<json.length;i++){
-//     console.log(json[i]);
-// }
+// async function processLineByLine() {
+//     let csv = []
+//     const fileStream = fs.createReadStream('testCsv.csv');
+  
+//     const rl = readline.createInterface({
+//       input: fileStream,
+//       crlfDelay: Infinity
+//     });
+//     // Note: we use the crlfDelay option to recognize all instances of CR LF
+//     // ('\r\n') in input.txt as a single line break.
+  
+//     for await (const line of rl) {
+//       // Each line in input.txt will be successively available here as `line`.
+//       console.log(`Line from file: ${line}`);
+//       csv.push(line)
+     
+//     }
+//     console.log("Inside",csv)
+//     return csv
+//   }
+  
+//   export async function m(formData){
+//     console.log("hello M",formData)
+//       let csv = []
+//       await processLineByLine().then((res)=>{
+//         console.log(`--> ${res}`)
+//         csv = res
+//       });
+
+//       console.log("array is:: ",csv)
+
+
+// var attrs = csv.splice(0,1);
+
+// var result = csv.map(function(row) {
+//   var obj = {};
+//   var rowData = row.split(';');
+//   attrs[0].split(';').forEach(function(val, idx) {
+//     obj = constructObj(val, obj, rowData[idx]);
+//   });
+//   return obj;
+// })
+// function constructObj(str, parentObj, data) {
+//     if(str.split('/').length === 1) {
+//       parentObj[str] = data;
+//       return parentObj;
+//     }
+
+//     var curKey = str.split('/')[0];
+//     if(!parentObj[curKey])
+//       parentObj[curKey] = {};
+//     parentObj[curKey] = constructObj(str.split('/').slice(1).join('/'), parentObj[curKey], data);
+//     return parentObj;
+//   }
+
+// console.log(`Result is:: ${JSON.stringify(result)}`)
+
+     
+//   }
+//   m()
+
+
+
+
+
+
