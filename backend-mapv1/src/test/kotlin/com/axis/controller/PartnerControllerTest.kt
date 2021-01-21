3c58cf4382c4320d1df26d019d62a88ae1b991f6
Aishwarya0710 <aishwaryamagre@gmail.com>
2021-01-21 09:52:05 +0530

    (update)testcases

--- backend-mapv1/src/test/kotlin/com/axis/controller/PartnerControllerTest.kt
+++ backend-mapv1/src/test/kotlin/com/axis/controller/PartnerControllerTest.kt
@@ -120,7 +120,54 @@ class PartnerControllerTest
 
     }
 
+    @Test
+    @Throws(Exception::class)
+    fun getQuoteTest() {
+        val objectMapper = ObjectMapper()
+        val partner=Partner(138, "https://tinyurl.com/ya22aob9", "AXIS MOTOR", "Get Affordable Insurance Plans for Car, Bike, Health & Travel with the Trust of TATA. Quick & Hassle-Free Online Process with No Paperwork. 5 Cr+ Happy Customers. Get Quote. Zero Touch Insurance. Policy in 3 iteps. Comprehensive Plans.",
+                "CarInsurance", "http://localhost:8089/motor-axis-insure/quotes", "{\"fields\": [ { \"label\":\"City\", \"id\":\"tex\", \"type\":\"text\", \"name\":\"city\", \"placeholder\":\" Enter City eg (Delhi or Mumbai)\",\"validation\":{\"required\":\"city is mandatory\" }}, { \"label\":\"RTO\", \"id\":\"tex\", \"type\":\"text\", \"name\":\"RTO\", \"placeholder\":\" Enter RTO eg (DL01 or MH01)\" ,\"validation\":{\"required\":\"RTO is mandatory\" }}, { \"label\":\"Brand Name\", \"id\":\"tex\", \"type\":\"text\", \"name\":\"brandName\", \"placeholder\":\" Enter Brand Name eg (Hyundai or Audi)\",\"validation\":{\"required\":\"Brand Name is mandatory\" } }, { \"label\":\"Fuel Type\", \"id\":\"tex\", \"type\":\"text\", \"name\":\"fuelType\", \"placeholder\":\" Enter Fuel type eg (Petrol or Diesel)\",\"validation\":{\"required\":\"Fuel Type is mandatory\" } }, { \"label\":\"Model Year\", \"id\":\"tex\", \"type\":\"text\", \"name\":\"modelYear\", \"placeholder\":\" Enter Model Year\",\"validation\":{\"required\":\"Model is mandatory\" } } ]}",
+                true, "{\"City\":\"city\",\"RTO\":\"RTO\",\"Name\":\"brandName\",\"Type\":\"fuelType\",\"Year\":\"modelYear\",\"Phone\":\"phoneNumber\"}");
 
+        val formData:String=""
+        mvc?.perform(MockMvcRequestBuilders
+                .post("/test/get-quote/138").content(objectMapper.writeValueAsString(formData))
+                .accept(MediaType.APPLICATION_ATOM_XML)
+                .contentType(MediaType.APPLICATION_ATOM_XML))
+                ?.andDo(MockMvcResultHandlers.print())
+                ?.andExpect(MockMvcResultMatchers.status().isOk());
+
+
+    }
+
+    @Test
+    fun getAdditionalFieldsTest()
+    {
+        mvc?.perform(MockMvcRequestBuilders
+                .get("/test/get-additional-fields/138")
+                .accept(MediaType.APPLICATION_ATOM_XML))
+                ?.andDo(MockMvcResultHandlers.print())
+                ?.andExpect(MockMvcResultMatchers.status().isOk());
+
+
+    }
+    @Test
+    @Throws(Exception::class)
+    fun getQuotesTest() {
+        val objectMapper = ObjectMapper()
+        val partner=Partner(138, "https://tinyurl.com/ya22aob9", "AXIS MOTOR", "Get Affordable Insurance Plans for Car, Bike, Health & Travel with the Trust of TATA. Quick & Hassle-Free Online Process with No Paperwork. 5 Cr+ Happy Customers. Get Quote. Zero Touch Insurance. Policy in 3 iteps. Comprehensive Plans.",
+                "CarInsurance", "http://localhost:8089/motor-axis-insure/quotes", "{\"fields\": [ { \"label\":\"City\", \"id\":\"tex\", \"type\":\"text\", \"name\":\"city\", \"placeholder\":\" Enter City eg (Delhi or Mumbai)\",\"validation\":{\"required\":\"city is mandatory\" }}, { \"label\":\"RTO\", \"id\":\"tex\", \"type\":\"text\", \"name\":\"RTO\", \"placeholder\":\" Enter RTO eg (DL01 or MH01)\" ,\"validation\":{\"required\":\"RTO is mandatory\" }}, { \"label\":\"Brand Name\", \"id\":\"tex\", \"type\":\"text\", \"name\":\"brandName\", \"placeholder\":\" Enter Brand Name eg (Hyundai or Audi)\",\"validation\":{\"required\":\"Brand Name is mandatory\" } }, { \"label\":\"Fuel Type\", \"id\":\"tex\", \"type\":\"text\", \"name\":\"fuelType\", \"placeholder\":\" Enter Fuel type eg (Petrol or Diesel)\",\"validation\":{\"required\":\"Fuel Type is mandatory\" } }, { \"label\":\"Model Year\", \"id\":\"tex\", \"type\":\"text\", \"name\":\"modelYear\", \"placeholder\":\" Enter Model Year\",\"validation\":{\"required\":\"Model is mandatory\" } } ]}",
+                true, "{\"City\":\"city\",\"RTO\":\"RTO\",\"Name\":\"brandName\",\"Type\":\"fuelType\",\"Year\":\"modelYear\",\"Phone\":\"phoneNumber\"}");
+
+        val formData:String=""
+        mvc?.perform(MockMvcRequestBuilders
+                .post("/test/get-quotes/CarInsurance/138").content(objectMapper.writeValueAsString(formData))
+                .accept(MediaType.APPLICATION_ATOM_XML)
+                .contentType(MediaType.APPLICATION_ATOM_XML))
+                ?.andDo(MockMvcResultHandlers.print())
+                ?.andExpect(MockMvcResultMatchers.status().isOk());
+
+
+    }
 
 
 }
